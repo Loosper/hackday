c87523df1c5e8689be057b4ddee6631e6ba46d25
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2019-11-02 12:41:48 +0000

    Refactor into a hosts class

---
+++ server/hosts.py
@@ -0,0 +1,39 @@
+import socket
+import os
+import json
+import asynchat
+
+
+class Hosts:
+    _hosts = set()
+    _removed = set()
+
+    def sync_with_files(self):
+        with open(os.path.join(__file__, "../hosts.txt"), "r+") as hostsfile:
+            hostslist = hostsfile.readlines()
+            for line in hostslist:
+                line = line.strip()
+                if len(line) > 0:
+                    self._hosts.add(line)
+
+            self._hosts.difference_update(self._removed)
+            hostsfile.seek(0)
+            hostsfile.write('\n'.join(self)+'\n')
+            hostsfile.truncate()
+
+    def add(self, item):
+        self._hosts.add(item)
+        if item in self._removed:
+            self._removed.remove(item)
+        self.sync_with_files()
+
+    def remove(self, item):
+        if item in self._hosts:
+            self._hosts.remove(item)
+        self._removed.add(item)
+        self.sync_with_files()
+
+    def __iter__(self):
+        listed = list(self._hosts)
+        listed.sort()
+        return iter(listed)
